b2aeddc1cd5a8502a2134f268c82e2fe4c6eb802
AhlinGregor <150283281+AhlinGregor@users.noreply.github.com>
2024-12-11 22:44:06 +0100

    First commit
    add switch statement to run different programms depending on the arguments

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,20 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        switch (args[1]) {
+            case "1":
+                sequentialSollution();
+                break;
+            case "2":
+                parallelSollution();
+                break;
+            case "3":
+                cudaSollution();
+                break;
+        }
     }
+
+    public static void sequentialSollution() {}
+    public static void parallelSollution() {}
+    public static void cudaSollution() {}
 }
